d8143e914dd5dd9af59b418a3b01f660175f5aa3
Nicholas Lee <nlee2000jh@gmail.com>
2024-04-10 04:56:12 +0800

    Created NewRandomForest.py
    Uses expanding window to do 8x 1 step forecast on real time data

---
+++ NewRandomForest.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from GetData import get_data
+# from PlotGraphs import *
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+
+def random_forest(year, quarter):
+    real_time_X, real_time_y, latest_X_train, latest_y_train, latest_X_test, latest_y_test, curr_year, curr_quarter = get_data(year, quarter)
+    top_n_variables = len(real_time_X.columns) // 3
+
+    '''
+    No need train test split because we are comparing against actual values from latest vintage directly
+    '''
+
+    # Train Random Forest Model
+    rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
+    rf_model.fit(real_time_X, real_time_y.values.ravel())
+
+    # Get feature importance scores
+    feature_importance = rf_model.feature_importances_
+    feature_importance_df = pd.DataFrame({'Feature': real_time_X.columns, 'Importance': feature_importance})
+    feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
+    print(feature_importance_df)
+
+    # Choose top N most important variables
+    selected_variables = feature_importance_df.head(top_n_variables)['Feature'].tolist()
+    print(f"Top {top_n_variables} Variables Selected:")
+    print(selected_variables)
+
+    # Expanding window
+    y_pred = []
+    # h step forecast
+    for i in range(12):
+        real_time_X_loop, real_time_y_loop, latest_X_train_loop, latest_y_train_loop, latest_X_test_loop, latest_y_test_loop, curr_year, curr_quarter = get_data(year, quarter)
+        # Adjust selected variables names to match each next quarter and year
+        temp = []
+        for var in selected_variables:
+            temp.append(f'{var[:-4]}{year[-2:]}Q{quarter}')
+        selected_variables = temp
+        # Train model and make 1 step forecast
+        rf_model_selected = RandomForestRegressor(n_estimators=100, random_state=42)
+        rf_model_selected.fit(real_time_X_loop[selected_variables], real_time_y_loop.values.ravel())
+        y_pred.append(rf_model_selected.predict(real_time_X_loop[selected_variables].iloc[-1:, :])[0])
+        # Step to next vintage
+        quarter = str(int(quarter) + 1)
+        if quarter > "4":
+            year = str(int(year) + 1)
+            quarter = "1"
+
+    # MSFE
+    rmsfe = mean_squared_error(y_pred, latest_y_test) ** 0.5
+    print(f'RMSFE: {rmsfe}')
+    '''
+    We are making one-step ahead forecasts iteratively, where each prediction depends only on the previous data and not on the entire dataset.
+    LOOCV typically involves training the model on all but one data point and then validating on the omitted data point.
+    However, in our iterative forecasting setup, each prediction is made sequentially and the training data continually changes.
+    Therefore, we use the metric RMSFE to assess the performance of the model.
+    '''
+
+    # Plots
+    CI = [0.57, 0.842, 1.282] # 50, 60, 80% predictional interval
+    def plot_forecast_real_time(data, forecast, CI, modelName):
+        fig, ax = plt.subplots(figsize=(4,4))  # Create a new figure and set the size
+        ax.plot(data.index, data.values, label='Unrevised Real Time Data', color='blue')
+        ax.plot(forecast.index, forecast.values, label='Forecast', color='red')
+        for i, ci in enumerate(CI):
+            alpha = 0.5 * (i + 1) / len(CI)
+            lower_bound = forecast - ci * forecast.std()
+            upper_bound = forecast + ci * forecast.std()
+            ax.fill_between(forecast.index, lower_bound, upper_bound, color='blue', alpha=alpha)
+
+        ax.set_xlim([data.index[-20], forecast.index[-1]])
+        ax.xaxis.set_major_locator(MaxNLocator(5))
+        ax.set_title(f'{modelName} Forecast with Real-Time Data')
+        ax.set_xlabel('Year:Quarter')
+        ax.set_ylabel('rGDP')
+        ax.legend()
+        plt.show()
+        plt.close(fig)
+    def plot_forecast_vintage(data, forecast, CI, modelName):
+        fig, ax = plt.subplots(figsize=(4,4))  # Create a new figure and set the size
+        ax.plot(data.index, data.values, label='Revised Vintage Data', color='blue')
+        ax.plot(forecast.index, forecast.values, label='Forecast', color='red')
+        for i, ci in enumerate(CI):
+            alpha = 0.5 * (i + 1) / len(CI)
+            lower_bound = forecast - ci * forecast.std()
+            upper_bound = forecast + ci * forecast.std()
+            ax.fill_between(forecast.index, lower_bound, upper_bound, color='blue', alpha=alpha)
+        
+        ax.set_xlim([data.index[-20], forecast.index[-1]])
+        ax.xaxis.set_major_locator(MaxNLocator(5))
+        ax.set_title(f'{modelName} Forecast with Revised Vintage Data')
+        ax.set_xlabel('Year:Quarter')
+        ax.set_ylabel('rGDP')
+        ax.legend()
+        plt.show()
+        plt.close(fig)
+    y_pred = pd.Series(y_pred)
+    y_pred.index = latest_y_test.index
+    real_time_plot = plot_forecast_real_time(real_time_y[1:], y_pred, CI, "RF Model")
+    latest_plot = plot_forecast_vintage(latest_y_train[1:], latest_y_test.iloc[:, 0], CI, "RF Model")
+
+    selected_variables_final = [var[:-4] for var in selected_variables]
+    importance_values = feature_importance_df["Importance"].tolist()
+    selected_variables_importance_dict = dict(zip(selected_variables_final, importance_values))
+    return selected_variables_importance_dict, rmsfe, real_time_plot, latest_plot
