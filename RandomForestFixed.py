2252123a5f44cf811af88bc81b7fe9880d50695b
Nicholas Lee <nlee2000jh@gmail.com>
2024-04-12 04:15:04 +0800

    Update RandomForestFixed.py
    Plot and choose optimal number of trees

--- RandomForestFixed.py
+++ RandomForestFixed.py
@@ -6,15 +6,39 @@ from GetData import get_data
 from PlotGraphs import *
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
 
 def random_forest(year,quarter):
     real_time_X, real_time_y, latest_X_train, latest_y_train, latest_X_test, latest_y_test, curr_year, curr_quarter = get_data(year, quarter)
     top_n_variables = len(real_time_X.columns) // 10
-    rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
-    rf_model_selected = RandomForestRegressor(n_estimators=100, random_state=42)
+
+    # region
+    # # Define a range of n_estimators values to try
+    # n_estimators_range = [5, 10, 15, 20, 30]
+    # # Initialize empty lists to store results
+    # mean_scores = []
+    # std_scores = []
+    # # Perform grid search with cross-validation
+    # for n_estimators in n_estimators_range:
+    #     rf = RandomForestRegressor(n_estimators=n_estimators, random_state=42)
+    #     scores = cross_val_score(rf, real_time_X, real_time_y.values.ravel(), cv=5, scoring='neg_mean_squared_error')
+    #     mean_scores.append(np.mean(scores))
+    #     std_scores.append(np.std(scores))
+    # print(mean_scores, std_scores)
+    # # Plot the mean scores
+    # plt.errorbar(n_estimators_range, mean_scores, yerr=std_scores, fmt='-o', color='b')
+    # plt.xlabel('n_estimators')
+    # plt.ylabel('Mean Squared Error')
+    # plt.title('Grid Search Results')
+    # plt.grid(True)
+    # plt.show()
+    # endregion
+
+    rf_model = RandomForestRegressor(n_estimators=10, random_state=42)
 
     y_pred = []
-    #print(real_time_X)
+    rmsfe = []
     
     for i in range(1,9):
         # Loop 1, use xt-1 for a values to find yt, then append to pred
@@ -30,9 +54,6 @@ def random_forest(year,quarter):
         real_time_X_lagged = real_time_X_lagged.iloc[i:]
         real_time_Y_chopped = real_time_y[i:]
 
-        
-        #print(real_time_X_lagged)
-
         # lag x by i, then fit into model
         rf_model.fit(real_time_X_lagged, real_time_Y_chopped.values.ravel())
         
@@ -43,20 +64,26 @@ def random_forest(year,quarter):
         selected_variables = feature_importance_df.head(top_n_variables)['Feature'].tolist()
         
         # Random forest with the top 6 again
+        rf_model_selected = RandomForestRegressor(n_estimators=10, random_state=42)
         rf_model_selected.fit(real_time_X_lagged[selected_variables], real_time_Y_chopped.values.ravel())
         
         # Forecast yt_j
         y_pred.append(rf_model_selected.predict(real_time_X_lagged[selected_variables].iloc[-1:, :])[0])
-     
-     # RMSFE of 1 step ahead forecast
-    rmsfe = round(mean_squared_error(y_pred, latest_y_test) ** 0.5,3)
-    #print(f'RMSFE: {rmsfe}')
+
+        # Backtesting
+        backtesting_predictions = []
+        for row in range(len(real_time_X_lagged)):
+            backtesting_predictions.append(rf_model_selected.predict(real_time_X_lagged[selected_variables].iloc[row:row+1, :])[0])
+        backtesting_actual = latest_y_train.iloc[i:, 0].to_list()
+        rmsfe.append(mean_squared_error(backtesting_predictions, backtesting_actual) ** 0.5)
+    print(rmsfe)
     
+    # Plots
     CI = [0.57, 0.842, 1.282] # 50, 60, 80% predictional interval
-    
     y_pred = pd.Series(y_pred) #adding this into output
     y_pred.index = latest_y_test.index
     real_time_plot = plot_forecast_real_time(real_time_y[1:], y_pred, latest_y_test, CI, "RF Model")
 
     return rmsfe, real_time_plot, y_pred
-#random_forest("2012", "3")
+
+# random_forest("2012", "3")
